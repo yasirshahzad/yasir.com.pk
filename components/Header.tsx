3b2b3dfb5bc6ec7395ffda005cf9e6614e82cdd3
Muhammad Yasir <shahzadyasir.com.pk>
2025-07-14 08:08:11 +0500

    build

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
       <Link href="/" aria-label={siteMetadata.headerTitle}>
         <div className="flex items-center justify-between">
           <div className="mr-3">
-            <Image src={"/fav.png"} alt="Logo" width={45} height={45} className='rounded-sm' />
+            <Image src={'/fav.png'} alt="Logo" width={45} height={45} className="rounded-sm" />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
             <div className="hidden h-6 text-2xl font-semibold sm:block">
